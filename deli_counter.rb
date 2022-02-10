e9f91e68790cfbf9961dbbaf653a2475453e819b
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-09 20:55:17 -0500

    commit tree a55a88f83a8325efce317beee4c94f4418601230

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,44 @@
-# Write your code here.
 require "pry"
 
-def line (line)
-    katz_deli = []
-    if katz_deli.empty?
-        puts "The line is currently empty."
-    else 
-        counter = 1
-        deliLine = []
-        line.each do |patron|
-            deliLine << "The line is currently: #{counter}. #{patron}"
-            counter += 1
-        end
-        puts deliLine
-    end
+# def line (line)
+#     katz_deli = []
+#     if line.empty?
+#         puts "The line is currently empty."
+#     else 
+#         counter = 1
+#         deliLine = []
+#         introSentence = "The line is currently:"
+#         line.each do |patron|
+#             deliLine << " #{counter}. #{patron}"
+#             counter += 1
+#         end
+#         puts introSentence + deliLine.join()
+#     end
+# end
 
+def line(queue)
+    if queue.length == 0
+        puts "The line is currently empty."
+    else
+        counter = 1
+        output = "The line is currently: "
+        queue.each do |patron|
+           output += "#{counter}. #{patron} "
+           counter += 1
+        end
+        puts output[0...-1]
+    end
+end
+
+def take_a_number(queue, patron)
+   queue << patron
+   puts "Welcome, #{patron}. You are number #{queue.length} in line."
+end
+
+def now_serving(queue)
+    if queue.length != 0
+        puts "Currently serving #{queue.shift()}."
+    else
+        puts "There is nobody waiting to be served!"
+    end
 end
