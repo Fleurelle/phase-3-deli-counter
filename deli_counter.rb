a21973cc45843ac4cb4bc541aa53e6bc32969426
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-09 19:38:14 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+require "pry"
+
+def line (line)
+    katz_deli = []
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else 
+        counter = 1
+        deliLine = []
+        line.each do |patron|
+            deliLine << "The line is currently: #{counter}. #{patron}"
+            counter += 1
+        end
+        puts deliLine
+    end
+
+end
